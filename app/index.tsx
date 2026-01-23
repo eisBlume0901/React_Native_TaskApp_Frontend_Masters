50b5bb6c6f3e1fc190b39d3592986cc2e1154f2e
eisBlume0901 <anecito.marykate09@gmail.com>
2026-01-23 22:29:22 +0800

    Converted the View into Scrollable (so that long list of information can be viewed properly). Also learned about sticky headers.

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, TextInput } from "react-native";
+import { StyleSheet, View, TextInput, ScrollView } from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { useState } from "react";
@@ -13,6 +13,16 @@ const initialList: ShoppingListItem[] = [
   { id: "1", name: "Banana" },
   { id: "2", name: "Cream Cheese" },
   { id: "3", name: "Sourdough Bread" },
+  { id: "4", name: "Greek Yogurt"},
+  { id: "5", name: "Vanilla Extract"},
+  { id: "6", name: "Crackers" },
+  { id: "7", name: "Rolled Oats" },
+  { id: "8", name: "Lettuce" },
+  { id: "9", name: "Onions" },
+  { id: "10", name: "Scallions" },
+  { id : "11", name: "Cookies" },
+  { id : "12", name: "Cherries" },
+  { id : "13", name: "Mixed Nuts" },
 ];
 
 export default function Index() {
@@ -30,7 +40,15 @@ export default function Index() {
     }
   };
   return (
-    <View style={styles.container}>
+    // contentContainerStyle - useful when styling CONTENTS inside a Scrollable View
+    // such as padding, gap, and alignment
+    // style - useful to style the Scrollable View CONTAINER
+    // such as size, flex, background
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+    >
       <TextInput
         placeholder="E.g. Bread"
         style={styles.textInput}
@@ -44,9 +62,8 @@ export default function Index() {
       {shoppingList.map((item) => (
         <ShoppingListItem name={item.name} key={item.id} />
       ))}
-
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -56,6 +73,9 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colorWhite,
     paddingTop: 12,
   },
+  contentContainer: {
+    paddingBottom: 24,
+  },
   textInput: {
     borderColor: theme.colorLightGrey,
     borderWidth: 2,
@@ -64,5 +84,6 @@ const styles = StyleSheet.create({
     marginBottom: 12,
     fontSize: 18,
     borderRadius: 50,
+    backgroundColor: theme.colorWhite,
   },
 });
